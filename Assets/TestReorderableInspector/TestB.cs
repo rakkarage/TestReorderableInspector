c6a08e76f1484b7ac9467a6103bf15f3cc85ce7a
Rakka Rage <rakkarage@gmail.com>
2018-05-23 22:04:33 -0400

    X

--- Assets/TestReorderableInspector/TestB.cs
+++ Assets/TestReorderableInspector/TestB.cs
@@ -1,5 +1,4 @@
-﻿using Homebrew;
-using System;
+﻿using System;
 using UnityEngine;
 namespace UnityEngine.Tilemaps
 {
@@ -7,12 +6,7 @@ namespace UnityEngine.Tilemaps
 	[Serializable]
 	public class TestB : ScriptableObject
 	{
-		[Foldout("test0")]
-		public int test0;
-
-		[Foldout("A")]
 		public int[] test;
-		[Foldout("AA")]
 		public TileOrientation orientation;
 	}
 }
