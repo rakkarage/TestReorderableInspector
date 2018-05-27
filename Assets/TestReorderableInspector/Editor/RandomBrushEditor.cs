128fc360c7223946dd7ac0bcf98ee2b7c53e7007
Rakka Rage <rakkarage@gmail.com>
2018-05-27 14:10:13 -0400

    x

--- Assets/TestReorderableInspector/Editor/RandomBrushEditor.cs
+++ Assets/TestReorderableInspector/Editor/RandomBrushEditor.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 namespace UnityEditor
 {
+	// https://github.com/SubjectNerd-Unity/ReorderableInspector
 	// if i remove this attribute it works in inspector and paint inspector
-	// and does not use this class at all
+	// because does not use this class at all
 	// but i need to use this attribute to link this other non gui related (preview etc) code to the brush
 	// so i use DrawDefaultInspector
 	// it works for my CustomPropertyDrawer for enum flags but does not work for ReorderableInspector?
+	// - [Reorderable] attribute does not work
+	// - cannot derive from ReorderableArrayInspector because i need to derive from GridBrushEditor
+	// is there any way to make it show ReorderableInspector for RandomTile AND RandomBrush?
+	// thanks
+	// https://github.com/rakkarage/TestReorderableInspector
 	[CustomEditor(typeof(RandomBrush))]
 	public class RandomBrushEditor : GridBrushEditor
 	{
