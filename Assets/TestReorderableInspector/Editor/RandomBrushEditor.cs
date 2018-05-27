cb82a94dcd49c7c3be7ce1976c420f9d3c3908f4
Rakka Rage <rakkarage@gmail.com>
2018-05-27 14:00:44 -0400

    still not working!?

--- Assets/TestReorderableInspector/Editor/RandomBrushEditor.cs
+++ Assets/TestReorderableInspector/Editor/RandomBrushEditor.cs
@@ -3,12 +3,23 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 namespace UnityEditor
 {
-	// [CustomEditor(typeof(RandomBrush))]
+	// if i remove this attribute it works in inspector and paint inspector
+	// and does not use this class at all
+	// but i need to use this attribute to link this other non gui related (preview etc) code to the brush
+	// so i use DrawDefaultInspector
+	// it works for my CustomPropertyDrawer for enum flags but does not work for ReorderableInspector?
+	[CustomEditor(typeof(RandomBrush))]
 	public class RandomBrushEditor : GridBrushEditor
 	{
 		public override void PaintPreview(GridLayout gridLayout, GameObject brushTarget, Vector3Int position)
 		{
-			Debug.Log("TEST!");
+			Debug.Log("PaintPreview!");
 		}
+		private void GUI()
+		{
+			DrawDefaultInspector();
+		}
+		public override void OnPaintInspectorGUI() => GUI();
+		public override void OnInspectorGUI() => GUI();
 	}
 }
